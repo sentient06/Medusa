349507f8fa015a63e0ce658a54b7e9dd393d39a5
Giancarlo Mariot <sentient.06@gmail.com>
2012-04-10 14:59:18 +0100

    Initial Commit

---
+++ Medusa/AppDelegate.h
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.h
+//  Medusa
+//
+//  Created by Giancarlo Mariot on 10/04/2012.
+//  Copyright (c) 2012 Giancarlo Mariot. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+
+- (IBAction)saveAction:(id)sender;
+
+@end
