b2da1110e8f0bb7c9373ea590c79cdcd868658db
Giancarlo Mariot <sentient.06+dev@gmail.com>
2016-10-25 23:25:43 +0100

    Fixed issues with bootable flag, ready for testing before release of RC 5.

---
+++ Medusa/TransformerControllers/ImageTransformers/BoolValueToBootTransformer.h
@@ -0,0 +1,13 @@
+//
+//  BoolValueToBootTransformer.h
+//  Medusa
+//
+//  Created by Giancarlo Mariot on 25/10/2016.
+//  Copyright (c) 2016 Giancarlo Mariot. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BoolValueToBootTransformer : NSValueTransformer
+
+@end
---
+++ Medusa/TransformerControllers/ImageTransformers/BoolValueToBootTransformer.m
@@ -0,0 +1,13 @@
+//
+//  BoolValueToBootTransformer.m
+//  Medusa
+//
+//  Created by Giancarlo Mariot on 25/10/2016.
+//  Copyright (c) 2016 Giancarlo Mariot. All rights reserved.
+//
+
+#import "BoolValueToBootTransformer.h"
+
+@implementation BoolValueToBootTransformer
+
+@end
