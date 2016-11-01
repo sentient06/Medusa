bee7fb13ff79ace77a49014900e6837807b86799
Giancarlo Mariot <sentient.06+dev@gmail.com>
2016-11-01 19:05:46 +0000

    v1.2.0 RC 5.

--- DMG/create-dmg.sh
+++ DMG/create-dmg.sh
@@ -3,7 +3,7 @@
 mkdir -p ~/Development/Cocoa/Medusa/DMG_TEMP/
 cp -R ~/Development/Cocoa/Medusa/DerivedData/Build/Products/Release/Medusa.app ~/Development/Cocoa/Medusa/DMG_TEMP/ ;
 ~/Development/Bash/create-dmg/create-dmg \
---volname "Medusa 1.2RC5" \
+--volname "Medusa 1.2.0RC5" \
 --volicon ~/Development/Cocoa/Medusa/DMG/medusa_dmg/dmg.icns \
 --background ~/Development/Cocoa/Medusa/DMG/medusa_dmg/dmg5.png \
 --window-pos 200 200 \
@@ -11,9 +11,9 @@ cp -R ~/Development/Cocoa/Medusa/DerivedData/Build/Products/Release/Medusa.app ~
 --text-size 10 \
 --text-position right \
 --icon-size 128 \
---icon Medusa.app 142 114 \
+--icon Medusa.app 144 116 \
 --hide-extension Medusa.app \
 --app-drop-link 416 186 \
-~/Desktop/Medusa.1.2rc5.dmg \
+~/Desktop/Medusa.1.2.0rc5.dmg \
 ~/Development/Cocoa/Medusa/DMG_TEMP ;
 rm -rf ~/Development/Cocoa/Medusa/DMG_TEMP/
